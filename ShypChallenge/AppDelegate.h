da1fdde051dad1dacc96a776d80d85d605a98864
benjaminy672 <benjaminy672@gmail.com>
2015-09-06 11:11:23 -0700

    Initial Commit

---
+++ ShypChallenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShypChallenge
+//
+//  Created by Jason Yonker on 9/6/15.
+//  Copyright (c) 2015 OMG Labs Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShypChallenge/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  ShypChallenge
+//
+//  Created by Jason Yonker on 9/6/15.
+//  Copyright (c) 2015 OMG Labs Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
